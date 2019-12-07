5e3bfd71aadf3366f35cc60939f96592ee6c2384
Real-cto <57210947+Real-cto@users.noreply.github.com>
2019-12-07 14:18:38 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,7 +1,7 @@
 export GITACCOUNT=
 
 export MYCLUSTER=tektonknativebeijing17
-export KUBECONFIG=
+export KUBECONFIG=/tmp/ic/AB/.bluemix/plugins/container-service/clusters/tektonknativebeijing17/kube-config-tok02-tektonknativebeijing17.yml
 
 export NAMESPACE=ctocto
 export EMAIL=
